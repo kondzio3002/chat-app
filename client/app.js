1ba10b53453748610a55d8d4a943f86b479059b1
Konrad Brożyna <kondzio3002@gmail.com>
2023-02-13 22:08:17 +0100

    Add function sendMessage and addMessage

--- client/app.js
+++ client/app.js
@@ -10,7 +10,7 @@ let userName = '';
 const login = e => {
   e.preventDefault();
   if(userNameInput.value.length == 0){
-    alert("Please enter a username");
+    alert('Please enter a username');
   } else {
     userName = userNameInput.value;
     loginForm.classList.remove('show');
@@ -19,3 +19,27 @@ const login = e => {
 };
 
 loginForm.addEventListener('submit', (e) => login(e));
+
+const addMessage = (author, content) => {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author == userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${author === userName ? 'You' : author}</h3>
+    <div class="message__content">${content}</div>
+  `
+  messagesList.appendChild(message);
+};
+
+const sendMessage = e => {
+  e.preventDefault();
+  if(messageContentInput.value.length == 0){
+    alert('Please enter a message');
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  }
+};
+
+addMessageForm.addEventListener('submit', (e) => sendMessage(e));
